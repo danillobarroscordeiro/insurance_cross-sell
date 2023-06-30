e2a9854a7e9d56d7ce855ce646e8b5e6c2379239
Danillo Barros <danillo_barros@live.com>
2023-06-30 15:44:44 -0300

    include handler.py and insurance.py

--- src/handler.py
+++ src/handler.py
@@ -1,21 +1,33 @@
-import pickle 
 import pandas as pd
 from flask import Flask, request, Response
-import insurance.insurance as insurance
-model = pickle.load(open('models/insurance_cross-sell_rf_model.pkl', 'rb') ) 
+import insurance
+import waitress
+import joblib
+import s3fs
+import os
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+s3 = s3fs.S3FileSystem(
+    anon=False, key=AWS_ACCESS_KEY_ID, secret=AWS_SECRET_ACCESS_KEY)
+
+path = 's3://insurance-cross-sell/'
+model = joblib.load(
+    s3.open(path + 'model.pkl', 'rb')) 
 
 #initializing API
 app = Flask(__name__)
 
-@app.route('/predict', methods=['POST'] )
+@app.route('/predict', methods=['POST'])
 def health_insurance_predict():
     test_json = request.get_json()
     
     if test_json: # there is data
         if isinstance(test_json, dict): #unique example
-            test_raw = pd.DataFrame(test_json, index=[0] )
+            test_raw = pd.DataFrame(test_json, index=[0])
         else: #multiple example
-            test_raw = pd.DataFrame(test_json, columns=test_json[0].keys() )
+            test_raw = pd.DataFrame(
+                test_json, columns=test_json[0].keys()
+            )
     
         #Instantiate Insurance class
         pipeline = insurance.Insurance()
@@ -33,7 +45,9 @@ def health_insurance_predict():
         df = pipeline.feature_selection(df)
 
         #prediction
-        df_response = pipeline.prediction(model,transformed_data = df,original_data= test_raw)
+        df_response = pipeline.prediction(
+            model,transformed_data = df,original_data= test_raw
+        )
 
         return df_response
     
@@ -41,5 +55,5 @@ def health_insurance_predict():
         return Response('{}', status=200, mimetype='application/json')
     
 if __name__ == '__main__':
-    app.run('0.0.0.0', debug=True)
-        
+    # app.run('0.0.0.0', debug=True)
+    waitress.serve(app,host='0.0.0.0', port=8080)
--- src/insurance/insurance.py
+++ src/insurance/insurance.py
@@ -4,22 +4,34 @@ import numpy as np
 from sklearn.compose import make_column_transformer
 from sklearn.preprocessing import RobustScaler, MinMaxScaler, LabelEncoder,OneHotEncoder
 from sklearn.ensemble import RandomForestClassifier
-from sklearn import model_selection, metrics
-from sklearn.metrics import make_scorer
 import math
-import pickle
+import joblib
 import inflection
-
+import s3fs
+import os
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+s3 = s3fs.S3FileSystem(
+    anon=False, key=AWS_ACCESS_KEY_ID, secret=AWS_SECRET_ACCESS_KEY)
 
 class Insurance(object):
     def __init__(self):
-        self.home_path = ''
-        self.vintage_scaler = pickle.load(open(self.home_path + 'features/vintage_scaler.pkl', 'rb') )
-        self.annual_premium_scaler = pickle.load(open(self.home_path + 'features/annual_premium_scaler.pkl', 'rb' ) )
-        self.annual_premium_per_month_scaler = pickle.load(open(self.home_path + 'features/annual_premium_per_month_scaler.pkl', 'rb') )
-        self.annual_premium_and_vintage_scaler = pickle.load(open(self.home_path + 'features/annual_premium_and_vintage_scaler.pkl', 'rb') )
-        self.age_scaler = pickle.load(open(self.home_path + 'features/age.pkl', 'rb') )
-        self.one_hot_encoding_scaler = pickle.load(open(self.home_path + 'features/one_hot_encoding_scaler.pkl', 'rb') )
+        self.home_path = 's3://insurance-cross-sell/'
+        self.vintage_scaler = joblib.load(
+            s3.open(self.home_path + 'vintage_scaler.pkl', 'rb')
+        )
+        self.annual_premium_scaler = joblib.load(
+            s3.open(self.home_path + 'annual_premium_scaler.pkl', 'rb')
+        )
+        self.annual_premium_and_vintage_scaler = joblib.load(
+            s3.open(self.home_path + 'annual_premium_and_vintage_scaler.pkl', 'rb')
+        )
+        self.age_scaler = joblib.load(
+            s3.open(self.home_path + 'age.pkl', 'rb')
+        )
+        self.one_hot_encoding_scaler = joblib.load(
+            s3.open(self.home_path + 'one_hot_encoding_scaler.pkl', 'rb')
+        )
         
     def data_cleaning(self,df):
         cols_old = df.columns.values
@@ -28,10 +40,14 @@ class Insurance(object):
 
         cols_new = list(map(snakecase, cols_old))
 
-        df.columns=cols_new
+        df.columns = cols_new
 
-        df['driving_license'] = df['driving_license'].apply(lambda x: 'Yes' if x == 1 else 'No')
-        df['previously_insured'] = df['previously_insured'].apply(lambda x: 'Yes' if x == 1 else 'No')
+        df['driving_license'] = df['driving_license'].apply(
+            lambda x: 'Yes' if x == 1 else 'No'
+        )
+        df['previously_insured'] = df['previously_insured'].apply(
+            lambda x: 'Yes' if x == 1 else 'No'
+        )
 
         return df
 
@@ -52,12 +68,20 @@ class Insurance(object):
         
         #Gender, previously_insured,vehicle_damage,driver_license - one hot encoding
         transformed = self.one_hot_encoding_scaler.transform(df)
-        transformed_df = pd.DataFrame(transformed, columns=self.one_hot_encoding_scaler.get_feature_names_out(), index=df.index)
-        df = pd.concat([df,transformed_df],axis=1).drop(['gender', 'previously_insured','vehicle_damage','driving_license'], axis=1)
+        transformed_df = pd.DataFrame(
+            transformed,
+            columns=self.one_hot_encoding_scaler.get_feature_names_out(),
+            index=df.index
+        )
+        df = pd.concat([df,transformed_df], axis=1).drop(
+                ['gender', 'previously_insured','vehicle_damage','driving_license'],
+                axis=1)
+    
 
         #Rescaling - MinMaxScaler
         df['vintage'] = self.vintage_scaler.transform(df[['vintage']].values)
-        df['annual_premium_and_vintage'] = self.annual_premium_and_vintage_scaler.transform(df[['annual_premium_and_vintage']].values)
+        df['annual_premium_and_vintage'] = self.annual_premium_and_vintage_scaler.transform(
+            df[['annual_premium_and_vintage']].values)
         df['age'] = self.age_scaler.transform(df[['age']].values)
 
         #Rescaling - RobustScaler
@@ -70,17 +94,22 @@ class Insurance(object):
         return df
             
     def feature_selection(self,df):
-        cols_selected = ['age','vehicle_age','annual_premium_and_vintage',
-         'onehotencoder__vehicle_damage_Yes','onehotencoder__vehicle_damage_No',
-         'policy_sales_channel','region_code', 'annual_premium','vintage']
+        cols_selected = [
+            'age','vehicle_age','annual_premium_and_vintage',
+            'onehotencoder__vehicle_damage_Yes','onehotencoder__vehicle_damage_No',
+            'policy_sales_channel','region_code', 'annual_premium','vintage'
+        ]
         df = df[cols_selected]
         
         return df
     
     def prediction(self, model,transformed_data,original_data):
         pred = model.predict_proba(transformed_data)
-        original_data['score'] = pred[:,1]
-        original_data = original_data.sort_values(by='score', ascending=False)
-        original_data['score'] = original_data['score'].apply(lambda x: str(round(x*100,2) ) + '%' )
+        original_data['response'] = pred[:,1]
+        original_data = original_data.sort_values(
+            by='response', ascending=False
+        )
+        original_data['response'] = original_data['response'].apply(
+            lambda x: str(round(x*100,2)) + '%')
             
         return original_data.to_json(orient='records', date_format='iso')
